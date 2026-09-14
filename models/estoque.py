7f066ab76ed66dddc97f0f2be4ef570067b337f8
Paulo Gonçalves <Paulogrdc@gmail.com>
2026-09-14 10:24:58 -0300

    fiz a classe Estoque

--- models/estoque.py
+++ models/estoque.py
@@ -1,4 +1,17 @@
 
 
 class Estoque: 
-    pass 
+    def __init__(self, produto:object, id:int , quantidade:int):
+         self.produto = produto
+         self._id=  id
+         self.quantidade = quantidade
+
+
+    def receber_produto(self, quant):
+         pass
+
+    def retirar_produto(self,quant):
+         pass 
+
+    def ind_estoque_baixo(self):
+         pass
