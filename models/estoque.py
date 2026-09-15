a44e99c952fd7fca3c2c4cf41da2bcc8b19e7e2d
Paulo Gonçalves <Paulogrdc@gmail.com>
2026-09-15 16:56:46 -0300

    fiz o property do Estoque

--- models/estoque.py
+++ models/estoque.py
@@ -7,6 +7,22 @@ class Estoque:
          self.quantidade = quantidade # -> fazer um property aqui 
 
 
+    @property 
+    def id(self): 
+        return self._id 
+
+    @id.setter
+    def id(self, valor): 
+          if valor <= 0: 
+               raise ValueError("Valor invalido! ")
+          else: 
+               self._populacao = valor
+
+    @property
+    def quantidade(self):
+         if self.quantidade < 0: 
+          print("Alerta! O valor do estoque não pode ser negativo!") 
+        
     def receber_produto(self, quant):
          pass
 
