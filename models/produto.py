a64ab1ed02c5b485d3d496d8e42c2d343ebd6c61
Paulo Gonçalves <Paulogrdc@gmail.com>
2026-09-15 16:46:23 -0300

    fiz o property de produto

--- models/produto.py
+++ models/produto.py
@@ -9,4 +9,21 @@ class Produto:
         self._estoque_min = estoque_min # -> fazer um porperty aqui 
 
 
-    
+    @property
+    def id(self):
+        return self._id 
+
+    @id.setter
+    def id(self,valor): 
+        if valor <= 0: 
+            raise ValueError("Valor invalido! ")
+        else: 
+            self._populacao = valor
+
+    @property
+    def estoque_min(self):
+        return self._estoque_min 
+
+    @estoque_min.setter
+    def estoque_min(self,valor): 
+        pass    # Para fazer esse property, eu preciso fazer uma analise de vendas do porduto
